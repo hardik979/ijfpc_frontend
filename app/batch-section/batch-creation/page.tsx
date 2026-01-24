049434799c3981cc679625f9c0e5ebba98f51402
ij f <sanjayshrivas0210@gmail.com>
2026-01-24 12:28:57 +0530

    correction in create batch route

--- app/batch-section/batch-creation/page.tsx
+++ app/batch-section/batch-creation/page.tsx
@@ -1,12 +1,21 @@
  "use client";
 
 import BatchUpdationPage from "@/components/batch/updatebatch";
+import { Suspense } from "react";
 
 export default function BatchCreatePage() {
 
   return(
     <>
+    <Suspense
+    fallback={
+        <div className="min-h-screen bg-slate-950 text-slate-100 flex items-center justify-center">
+          Loading...
+        </div>
+      }
+    >
     <BatchUpdationPage/>
+    </Suspense>
     </>
   )
 }
