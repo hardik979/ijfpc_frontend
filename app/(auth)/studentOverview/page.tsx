d5fa47e3cf88a6f89e51f218dc11e74a0195e7a5
Shanuthakur01 <bt075590@gmail.com>
2026-07-06 18:27:47 +0530

    new commit

--- app/(auth)/studentOverview/page.tsx
+++ app/(auth)/studentOverview/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 import StudentsListPage from "@/components/StudentList";
-
+//
 export default function StudentOverviewPage() {
   return (
     <>
       <StudentsListPage />
+      
     </>
   );
 }
